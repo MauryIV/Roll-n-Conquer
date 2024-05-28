be94fe55db99643cc5332f41cfa43cab07325c8a
JustinHinton <justgawd7@icloud.com>
2024-05-27 21:31:50 -0600

    updated the resolver

--- server/models/index.js
+++
@@ -1,4 +0,0 @@
-const User = require('./User');
-const Dice = require('./dice');
-
-module.exports = { User, Dice };
--- server/schemas/resolvers.js
+++
@@ -1,5 +0,0 @@
-const { User, Dice } = require("../models");
-const { signToken, AuthenticationError } = require("../utils/auth");
-const resolvers = {
-};
-module.exports = resolvers;
